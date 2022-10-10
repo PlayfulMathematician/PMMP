077b8c8d2fe704f97428cd5a6740de5db71c5c77
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-10-10 16:48:03 -0600

    Added test case

--- tests/test.py
+++ tests/test.py
@@ -2,4 +2,4 @@ from PMMP import main
 a = main.NumFunc(lambda g: main.Polynomial(-1, 0, 10, 10).solve(guess=g, iterations=100))
 import matplotlib.pyplot as plt
 plt.plot(list(range(-100, 100)), list(map(a, range(-100, 100))))
-plt.show(
+plt.show()
