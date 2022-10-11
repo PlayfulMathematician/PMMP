d7b1d9242902d19f8a412a673e825418ae54de63
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-10-11 16:50:11 -0600

    Changes

---
+++ src/PMMP/probability.py
@@ -0,0 +1,24 @@
+import random
+
+
+class FrequencySimulation:
+
+    def __init__(self, l):
+        self.l = l
+
+    @property
+    def frequency_data(self):
+        return {
+            j: len([i for i in self.l if i == j]) / len(self.l) for j in set(self.l)
+        }
+
+
+class DiceSimulation(FrequencySimulation):
+    def __init__(self, faces, roll_amt, sum_amt):
+        super().__init__(
+            [
+                sum([random.choice(faces) for q in range(0, sum_amt)])
+                for i in range(0, roll_amt)
+            ]
+        )
+
