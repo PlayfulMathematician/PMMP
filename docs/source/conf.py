f899b0c95c57a781cffaeb51187a0d2b9f92d30e
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-11-04 08:43:35 -0600

    Changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-import src.PMMP
+import PMMP
 project = 'PMMP'
 copyright = '2022, PlayfulMathematician'
 author = 'PlayfulMathematician'
