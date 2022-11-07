7bd459e65153c9dd6510ee163e28fcd5ca626d84
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-11-06 17:13:50 -0700

    Changes

--- src/PMMP/__init__.py
+++ src/PMMP/__init__.py
@@ -1,3 +1,11 @@
 __version__ = "0.1.1"
 __name__ = "PMMP"
 
+try:
+    from PMMP.functions import *
+    from PMMP.probability import *
+    from PMMP.numbers import *
+
+
+except ModuleNotFoundError:
+    pass
